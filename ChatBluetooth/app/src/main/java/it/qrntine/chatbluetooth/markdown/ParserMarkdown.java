fb6074fb469333eb012a2fab1c70211500f83be5
bramucciUni <adrianobramucci93@gmail.com>
2020-06-03 15:18:49 +0200

    Versione 1.16
    -Corretto problema riconoscimento list in MarkdownParser

--- ChatBluetooth/app/src/main/java/it/qrntine/chatbluetooth/markdown/ParserMarkdown.java
+++ ChatBluetooth/app/src/main/java/it/qrntine/chatbluetooth/markdown/ParserMarkdown.java
@@ -1,12 +1,27 @@
 package it.qrntine.chatbluetooth.markdown;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Pattern;
+
+/***
+ * Classe che consente il riconoscimento di semplici formattazioni markdown, traducendole in tag HTML leggibili
+ * in fase di setText della TextView.
+ *
+ *   Mark supportati:       Markdown:                 HTML:
+ *
+ *   GRASSETTO              ...**string**...          <b>string</b>
+ *   CORSIVO                ...*string*...            <em>string</em>
+ *   LIST                   nameList:*item1*item2...  <ul>nameList:<li>item1</li><li>item2</li>...</ul>
+ *   HEADER                 #string                   <h1>string</h1>
+ *
+ */
 
 public class ParserMarkdown {
 
     private static final String regexBold = "(.+?)*(\\*\\*)(.+?)(\\*\\*)(.+?)*"; // ..**...**..
     private static final String regexCursive = "(.+?)*(\\*)(.+?)(\\*)(.+?)*";  // ..*...*..
-    private static final String regexList = "(^[(.+?)(\\:)]((\\*){1}(.+?))*)"; // ..:*...*...
+    private static final String regexList = "(^(.+?)(\\:)((\\*){1}(.+?))*)"; // ..:*...*...
     private static final String regexHeader = "^(\\#{1})(.+?)*";    // #...
 
     //funzione che ritorna l'int da usare nella switch del metodo parsing
@@ -14,12 +29,19 @@ public class ParserMarkdown {
 
         String[] regExpressions = {regexBold, regexCursive, regexList, regexHeader};
 
+        List<Integer> resMatches = new ArrayList<>();   //uso una list per gestire eventuali casi di matches multipli
+
         for (int i=0; i<regExpressions.length; i++) {
             Pattern pattern = Pattern.compile(regExpressions[i]);
             if (pattern.matcher(stringa).matches()) {
-                return i; //deve controllare quale parser (case) chiamare e ritorna il numero, che poi uso per chiamare parsing
+                resMatches.add(i);
+                //return i; //usando regex piu' 'esclusive' e' possibile ritornare direttamente l'intero senza creare resMatches
             }
         }
+
+        if(resMatches.size() == 1) return resMatches.get(0);    //se ho trovato un singolo match, ritorno il numero corrispondente al case
+
+        if (resMatches.contains(1) && resMatches.contains(2)) return 2;   //caso in cui ho 2 match: in alcuni input la regexList(case: 2) e la regexCursive(case: 1) possono matchare entrambe, scelgo la list (return 2)
 
         return 10;	//caso default
     }
