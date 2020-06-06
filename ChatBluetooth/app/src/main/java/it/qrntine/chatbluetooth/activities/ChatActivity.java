95f40b49683d8fd2a468f9bf4d2dd028262e310a
stewieGIT <43251606+stewieGIT@users.noreply.github.com>
2020-06-06 19:44:38 +0200

    Versione 1.24

--- ChatBluetooth/app/src/main/java/it/qrntine/chatbluetooth/activities/ChatActivity.java
+++ ChatBluetooth/app/src/main/java/it/qrntine/chatbluetooth/activities/ChatActivity.java
@@ -130,24 +130,6 @@ public class ChatActivity extends AppCompatActivity implements SearchView.OnQuer
     }
 
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        finish();
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        if(session.getmBluetoothChatService().getmConnectedThread() != null)
-            session.getmBluetoothChatService().getmConnectedThread().setmHandler(mHandler);
-
-        if(session.getmBluetoothChatService().getmState() != 3){
-            holder.btnCSend.setEnabled(false);
-            holder.btnInviaMessaggio.setEnabled(false);
-        }
-    }
-
-    @Override
     public boolean onQueryTextSubmit(String query) {
         return false;
     }
